06233d1ff31bf373e44e86862e8fd51098c863fb
EyobABN <eyob.alamerew.belay@gmail.com>
2022-07-17 10:35:25 +0300

    Stop comparison at last element

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,13 +12,15 @@ void bubble_sort(int *array, size_t size)
 	int temp, swap_counter = 1;
 	size_t i;
 
-	if (array == NULL || size == 0)
+	if (array == NULL || size < 2)
 		return;
 	while (swap_counter != 0)
 	{
 		swap_counter = 0;
 		for (i = 0; i < size; i++)
 		{
+			if (i + 1 == size)
+				break;
 			if (array[i] > array[i + 1])
 			{
 				temp = array[i];
